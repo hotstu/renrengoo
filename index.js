ee4c695b835ff9c8b4eee2bf1c6de128436f5197
hglf <hgleifeng@qq.com>
2019-09-19 14:28:03 +0800

    update

--- index.js
+++ index.js
@@ -1,47 +1,46 @@
-const someHost = 'https://www.google.com'
+const someHost = "https://www.google.com";
 //replace this with you own subdomain!! 替换为你自己申请的subdomain!!
-const host = 'https://sink.hglf.workers.dev'
+const host = "https://_._.workers.dev";
 
 async function handleRequest(request) {
-  const { url: rawUrl } = request
-  const url = rawUrl.replace(host, someHost)
-  const { headers } = request
+  const { url: rawUrl } = request;
+  const url = rawUrl.replace(host, someHost);
+  const { headers } = request;
   const init = {
     headers: headers,
-    redirect: "manual",
-  }
-  const response = await fetch(url, init)
-  const results = await gatherResponse(response)
-  return new Response(results, copyMeta(response))
+    redirect: "manual"
+  };
+  const response = await fetch(url, init);
+  const results = await gatherResponse(response);
+  return new Response(results, copyMeta(response));
 }
 
-addEventListener('fetch', event => {
-  return event.respondWith(handleRequest(event.request))
-})
+addEventListener("fetch", event => {
+  return event.respondWith(handleRequest(event.request));
+});
 
 function copyMeta(response) {
   return {
     headers: response.headers,
     status: response.status
-  }
+  };
 }
 
 async function gatherResponse(response) {
-  console.log(response)
-  const { headers } = response
-  const contentType = headers.get('content-type')
-  if (contentType.includes('application/json')) {
-    return replace(await response.text())
-  } else if (contentType.includes('application/text')) {
-    return replace(await response.text())
-  } else if (contentType.includes('text/html')) {
-    return replace(await response.text())
+  const { headers } = response;
+  const contentType = headers.get("content-type");
+  if (contentType.includes("application/json")) {
+    return replace(await response.text());
+  } else if (contentType.includes("application/text")) {
+    return replace(await response.text());
+  } else if (contentType.includes("text/html")) {
+    return replace(await response.text());
   } else {
-    return await response.body
+    return await response.body;
   }
 }
 
 async function replace(html) {
-  return html.replace(someHost, host)
+  return html.replace(someHost, host);
 }
 
